d902716fdcaaf8b1a6c67a29ebb484add87f49c8
Gabriel Bonifácio Oliveira <gabriel.o.bonifacio@gmail.com>
2024-10-04 20:33:02 -0300

    Initial

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mininal_api.Dominio.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = default;
+        public string Senha { get; set; } = default;
+    }
+}
