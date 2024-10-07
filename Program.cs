dd36069048fb98e1ce9c22086cb8fda2ed98d9b2
Gabriel Bonifácio Oliveira <gabriel.o.bonifacio@gmail.com>
2024-10-07 07:20:50 -0300

    Insert: implementando o swagger

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using mininal_api.Infraestrutura.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServicos, AdministradorServico>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 {
@@ -41,4 +43,6 @@ app.MapPost("/login", async ([FromBody] LoginDTO loginDTO, IAdministradorServico
     return Results.Unauthorized();
 });
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
